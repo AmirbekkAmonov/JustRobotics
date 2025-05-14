9ebebf6fd14c2d907cebc098760d0cf16e075e4d
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-05-14 10:21:01 +0500

    contacts page

--- src/pages/Home/contacts.tsx
+++ src/pages/Home/contacts.tsx
@@ -1,7 +1,39 @@
+import { Select } from "antd";
+import "antd/dist/reset.css";
+import { useState } from "react";
 
 function contacts() {
+  const [selectedCourse, setSelectedCourse] = useState<string | undefined>(undefined);
+  const courses = [
+    "Работотехника",
+    "Живопись для взрослых",
+    "Живопись для детей",
+    "Шахмат",
+    "Английский",
+  ];
+
   return (
-    <div></div>
+    <div className="contacts" id="contact">
+      <div className="container contacts_container">
+          <h3>получите бесплатный
+          первый урок</h3>
+          <div className="contacts_input">
+            <input type="text"  placeholder="Ваше имя"/>
+            <input type="text" placeholder="Номер телефона" />
+            <Select
+              style={{ width: "100%", borderRadius: 9, fontSize: 22 }}
+              placeholder="Выберете курс"
+              value={selectedCourse}
+              onChange={setSelectedCourse}
+              options={courses.map((c) => ({ value: c, label: c }))}
+              size="large"
+              dropdownStyle={{ borderRadius: 9, fontSize: 22 }}
+            />
+            <input type="text"  placeholder="Возраст"/>
+            <button>получить</button>
+          </div>
+      </div>
+    </div>
   )
 }
 
